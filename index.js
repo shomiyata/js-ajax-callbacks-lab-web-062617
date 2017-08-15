8cd29c0936bdf4d1ba8258ccb3ef9a33517bfa8c
github username <github email address>
2017-08-15 18:20:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
-$(document).ready(function (){
+$(document).ready(function (){		
 });
+
+function searchRepositories(){
+		let searchTerms = window.$('#searchTerms').val()
+
+		let url = `https://api.github.com/search/repositories?q=${searchTerms}`;
+
+		$.get(url)
+			.done(function(data) {
+				let response = data.items
+				let responseHTML = `<ul>${response.map(repo => '<li>' + '<a href="' + repo.html_url + '">' + repo.name + '</a>' + ', ' + repo.description + ', ' + ', ' + repo.owner.login + ', ' + repo.owner.url + ', ' + repo.owner.avatar_url + ", " + '<a href="#" data-owner="' + repo.owner.login + '"data-repo="' + repo.name + '" onclick="showCommits(this)">' + 'Show Commits' + '</a>').join('')}</ul>`
+  				$('#results').html(responseHTML)
+  				}).fail(displayError())
+}
+
+function displayError(){
+	$("#errors").html("I'm sorry, there's been an error. Please try again.")
+}
+
+function formatCommit(r) {
+	return `<li>${r.author.login}, ${r.commit.author.name}, ${r.sha}, <img src='${r.committer.avatar_url}'></li>`
+}
+response[0].commit.author.name
+ // SHA, the author, the author's login, and the author's avatar as an image.
+
+function showCommits(ele){
+	let userName = ele.dataset.owner
+	let repoName = ele.dataset.repo
+	let url = `https://api.github.com/repos/${userName}/${repoName}/commits`
+
+	$.get(url)
+		.done(function(response) {
+			let responseHTML = `<ul>${response.map(commit => formatCommit(commit))}</ul>`
+			$('#details').html(responseHTML)
+		})
+}
